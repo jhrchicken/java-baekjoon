7a3d183a3f9927a9743255c25f224f26b34a7c10
jhrchicken <jhr.chicken@gmail.com>
2026-04-07 23:49:09 +0900

    1834번 최소공배수

---
+++ src/Q11689.java
@@ -0,0 +1,23 @@
+import java.util.Scanner;
+
+public class Q11689 {
+    public static void main(String args[]) {
+        Scanner sc = new Scanner(System.in);
+        long n = sc.nextLong();
+        long result = n;
+
+        for (long p = 2; p <= Math.sqrt(n); p++) {
+            if (n % p == 0) {
+                result = result - result / p;
+                while (n % p == 0) {
+                    n /= p;
+                }
+            }
+        }
+
+        if (n > 1) {
+            result = result - result / n;
+        }
+        System.out.println(result);
+    }
+}
