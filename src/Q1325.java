ee84e3dc783a33fee23310e48e4c76f5ee4b44c5
jhrchicken <jhr.chicken@gmail.com>
2026-04-09 00:33:12 +0900

    1325번 효율적인 해킹

---
+++ src/Q1325.java
@@ -0,0 +1,62 @@
+import java.io.*;
+import java.util.*;
+
+public class Q1325 {
+    static int N, M;
+    static boolean visited[];
+    static ArrayList<Integer>[] graph;
+    static int[] answer;
+
+    public static void main(String args[]) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        N = Integer.parseInt(st.nextToken());
+        M = Integer.parseInt(st.nextToken());
+        answer = new int[N + 1];
+
+        graph = new ArrayList[N + 1];
+        for (int i = 1; i <= N; i++) {
+            graph[i] = new ArrayList<>();
+        }
+
+        for (int i = 0; i < M; i++) {
+            st = new StringTokenizer(br.readLine());
+            int start = Integer.parseInt(st.nextToken());
+            int end = Integer.parseInt(st.nextToken());
+            graph[start].add(end);
+        }
+
+        for (int i = 1; i <= N; i++) {
+            visited = new boolean[N + 1];
+            BFS(i);
+        }
+
+        int max = 0;
+        for (int i = 1; i <= N; i++) {
+            max = Math.max(max, answer[i]);
+        }
+
+        for (int i = 1; i <= N; i++) {
+            if (answer[i] == max) {
+                System.out.print(i + " ");
+            }
+        }
+    }
+
+    public static void BFS(int node) {
+        Queue<Integer> queue = new LinkedList<Integer>();
+        queue.add(node);
+        visited[node] = true;
+
+        while (!queue.isEmpty()) {
+            int newNode = queue.poll();
+            for (int i : graph[newNode]) {
+                if (!visited[i]) {
+                    visited[i] = true;
+                    answer[i]++;
+                    queue.add(i);
+                }
+            }
+        }
+    }
+}
