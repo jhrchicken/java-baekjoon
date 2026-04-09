462886f92debd416bf0406bb6f36114a04da045c
jhrchicken <jhr.chicken@gmail.com>
2026-04-09 20:04:24 +0900

    1717번 집합의 표현

---
+++ src/Q1717.java
@@ -0,0 +1,62 @@
+import java.util.Scanner;
+
+public class Q1717 {
+    private static int[] parent;
+
+    public static void main(String args[]) {
+        Scanner sc = new Scanner(System.in);
+        int N = sc.nextInt();
+        int M = sc.nextInt();
+
+        parent = new int[N + 1];
+        for (int i = 0; i <= N; i++) {
+            parent[i] = i;
+        }
+
+        for (int i = 0; i < M; i++) {
+            int command = sc.nextInt();
+            int a = sc.nextInt();
+            int b = sc.nextInt();
+
+            if (command == 0) {
+                union(a, b);
+            }
+            else if (command == 1) {
+                if (isSame(a, b)) {
+                    System.out.println("YES");
+                }
+                else {
+                    System.out.println("NO");
+                }
+            }
+        }
+    }
+
+    public static void union(int a, int b) {
+        a = find(a);
+        b = find(b);
+        if (a != b) {
+            parent[b] = a;
+        }
+    }
+
+    public static int find (int a) {
+        if (a == parent[a]) {
+            return a;
+        }
+        else {
+            return parent[a] = find(parent[a]);
+        }
+    }
+
+    public static boolean isSame(int a, int b) {
+        a = find(a);
+        b = find(b);
+        if (a == b) {
+            return true;
+        }
+        else {
+            return false;
+        }
+    }
+}
