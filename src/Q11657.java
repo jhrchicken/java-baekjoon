586d687deed9ac43ab8a30276368e9f50d845ab7
jhrchicken <jhr.chicken@gmail.com>
2026-04-23 18:07:54 +0900

    11657번 타임머신으로 빨리 가기

---
+++ src/Q11657.java
@@ -0,0 +1,71 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.Arrays;
+import java.util.StringTokenizer;
+
+public class Q11657 {
+    static int N, M;
+    static long distance[];
+    static Edge edges[];
+
+    public static void main(String args[]) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        int N = Integer.parseInt(st.nextToken());
+        int M = Integer.parseInt(st.nextToken());
+
+        distance = new long[N + 1];
+        Arrays.fill(distance, Integer.MAX_VALUE);
+
+        edges = new Edge[M + 1];
+        for (int i = 0; i < M; i++) {
+            st = new StringTokenizer(br.readLine());
+            int start = Integer.parseInt(st.nextToken());
+            int end = Integer.parseInt(st.nextToken());
+            int time = Integer.parseInt(st.nextToken());
+            edges[i] = new Edge(start, end, time);
+        }
+
+        distance[1] = 0;
+        for (int i = 1; i < N; i++) {
+            for (int j = 0; j < M; j++) {
+                Edge edge = edges[j];
+                if (distance[edge.start] != Integer.MAX_VALUE && distance[edge.end] > distance[edge.start] + edge.time) {
+                    distance[edge.end] = distance[edge.start] + edge.time;
+                }
+            }
+        }
+
+        boolean minusCycle = false;
+        for (int i = 0; i < M; i++) {
+            Edge edge = edges[i];
+            if (distance[edge.start] != Integer.MAX_VALUE && distance[edge.end] > distance[edge.start] + edge.time) {
+                minusCycle = true;
+            }
+        }
+
+        if (!minusCycle) {
+            for (int i = 2; i <= N; i++) {
+                if (distance[i] == Integer.MAX_VALUE) {
+                    System.out.println("-1");
+                }
+                else {
+                    System.out.println(distance[i]);
+                }
+            }
+        }
+        else {
+            System.out.println("-1");
+        }
+    }
+}
+
+class Edge {
+    int start, end, time;
+    public Edge(int start, int end, int time) {
+        this.start = start;
+        this.end = end;
+        this.time = time;
+    }
+}
--- src/Q1753.java
+++ src/Q1753.java
@@ -1,3 +1,4 @@
+/*
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
@@ -85,3 +86,4 @@ class Edge implements Comparable<Edge> {
         }
     }
 }
+ */
