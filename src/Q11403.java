171e6ad12e4a05d4aa97123d9116dd46709b6e74
jhrchicken <jhr.chicken@gmail.com>
2026-04-23 19:16:48 +0900

    11403번 경로 찾기

---
+++ src/Q11403.java
@@ -0,0 +1,38 @@
+import java.io.*;
+import java.util.StringTokenizer;
+
+public class Q11403 {
+    static int N;
+    static int distance[][];
+
+    public static void main(String args[]) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+
+        N = Integer.parseInt(br.readLine());
+        distance = new int[N][N];
+        for (int i = 0; i < N; i++) {
+            StringTokenizer st = new StringTokenizer(br.readLine());
+            for (int j = 0; j < N; j++) {
+                distance[i][j] = Integer.parseInt(st.nextToken());
+            }
+        }
+
+        for (int k = 0; k < N; k++) {
+            for (int i = 0; i < N; i++) {
+                for (int j = 0; j < N; j++) {
+                    if (distance[i][k] == 1 && distance[k][j] == 1) {
+                        distance[i][j] = 1;
+                    }
+                }
+            }
+        }
+
+        for (int i = 0; i < N; i++) {
+            for (int j = 0; j < N; j++) {
+                System.out.print(distance[i][j] + " ");
+            }
+            System.out.println();
+        }
+    }
+}
