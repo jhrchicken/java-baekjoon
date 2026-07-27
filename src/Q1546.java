3f30235c83893c29baa468bd587ea333bc358f6e
harim <jhrchicken@gmail.com>
2026-07-27 21:33:35 +0900

    1546번 평균 구하기

--- src/Q1546.java
+++ src/Q1546.java
@@ -3,7 +3,6 @@ import java.util.Scanner;
 public class Q1546 {
     public static void main(String args[]) {
         Scanner sc = new Scanner(System.in);
-
         int N = sc.nextInt();
         int score[] = new int[N];
         long max = 0;
