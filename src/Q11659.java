cd30633fe6d5215b8699998a1031aea37dce8c28
harim <jhr.chicken@gmail.com>
2026-07-27 22:12:24 +0900

    11659번 구간 합 구하기

--- src/Q11659.java
+++ src/Q11659.java
@@ -12,7 +12,6 @@ public class Q11659 {
             S[i + 1] = S[i] + sc.nextInt();
         }
 
-
         for (int i = 0; i < M; i++) {
             int start = sc.nextInt();
             int end = sc.nextInt();
