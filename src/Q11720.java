da6bc39df75a99eb827e05f2a84e884a4ec4b996
harim <jhrchicken@gmail.com>
2026-07-27 20:54:49 +0900

    11720번 숫자의 합 구하기

--- src/Q11720.java
+++ src/Q11720.java
@@ -4,12 +4,10 @@ public class Q11720 {
     public static void main(String args[]) {
         Scanner sc = new Scanner(System.in);
         int N = sc.nextInt();
-
-        String sNum = sc.next();
-        char[] cNum = sNum.toCharArray();
+        String str = sc.next();
         int sum = 0;
-        for (int i = 0; i < cNum.length; i++) {
-            sum += cNum[i] - '0';
+        for (int i = 0; i < N; i++) {
+            sum += str.charAt(i) - '0';
         }
         System.out.println(sum);
     }
