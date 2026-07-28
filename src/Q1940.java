730a9ed036db290e9035e9365892083c3e6f15cd
harim <jhr.chicken@gmail.com>
2026-07-28 22:06:28 +0900

    1940번 주몽의 명령

--- src/Q1940.java
+++ src/Q1940.java
@@ -1,36 +1,40 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
 import java.util.Arrays;
-import java.util.Scanner;
+import java.util.StringTokenizer;
 
 public class Q1940 {
-    public static void main(String args[]) {
-        Scanner sc = new Scanner(System.in);
-        int N = sc.nextInt();
-        int M = sc.nextInt();
-        int A[] = new int[N];
+    public static void main(String args[]) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st;
 
+        int N = Integer.parseInt(br.readLine());
+        int M = Integer.parseInt(br.readLine());
+        int startIdx = 0;
+        int endIdx = N - 1;
+        int count = 0;
+        int[] A = new int[N];
+
+        st = new StringTokenizer(br.readLine());
         for (int i = 0; i < N; i++) {
-            A[i] = sc.nextInt();
+            A[i] = Integer.parseInt(st.nextToken());
         }
         Arrays.sort(A);
 
-        int count = 0;
-        int start_index = 0;
-        int end_index = N - 1;
-
-        while (start_index < end_index) {
-            if (A[start_index] + A[end_index] < M) {
-                start_index++;
+        while (startIdx < endIdx) {
+            if (A[startIdx] + A[endIdx] == M) {
+                count++;
+                startIdx++;
+                endIdx--;
             }
-            else if (A[start_index] + A[end_index] > M) {
-                end_index--;
+            else if (A[startIdx] + A[endIdx] < M) {
+                startIdx++;
             }
             else {
-                count++;
-                start_index++;
-                end_index--;
+                endIdx--;
             }
         }
-
         System.out.println(count);
     }
 }
--- src/tmp.java
+++ src/tmp.java
@@ -1,28 +1,33 @@
+import java.util.Arrays;
 import java.util.Scanner;
 
 public class tmp {
     public static void main(String args[]) {
         Scanner sc = new Scanner(System.in);
         int N = sc.nextInt();
+        int M = sc.nextInt();
+        int A[] = new int[N];
 
-        int count = 1;
-        int start_index = 1;
-        int end_index = 1;
-        int sum = 1;
+        for (int i = 0; i < N; i++) {
+            A[i] = sc.nextInt();
+        }
+        Arrays.sort(A);
 
-        while (end_index != N) {
-            if (sum == N) {
-                count++;
-                end_index++;
-                sum = sum + end_index;
-            }
-            else if (sum > N) {
-                sum = sum - start_index;
+        int count = 0;
+        int start_index = 0;
+        int end_index = N - 1;
+
+        while (start_index < end_index) {
+            if (A[start_index] + A[end_index] < M) {
                 start_index++;
             }
+            else if (A[start_index] + A[end_index] > M) {
+                end_index--;
+            }
             else {
-                end_index++;
-                sum = sum + end_index;
+                count++;
+                start_index++;
+                end_index--;
             }
         }
 
