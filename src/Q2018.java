7e38641c0a7c72084c3139af943d40cafba7b2ce
harim <jhr.chicken@gmail.com>
2026-07-28 21:34:09 +0900

    2018번 연속된 자연수의 합 구하기

--- src/Q2018.java
+++ src/Q2018.java
@@ -4,25 +4,24 @@ public class Q2018 {
     public static void main(String args[]) {
         Scanner sc = new Scanner(System.in);
         int N = sc.nextInt();
-
-        int count = 1;
-        int start_index = 1;
-        int end_index = 1;
         int sum = 1;
+        int count = 1;
+        int startIdx = 1;
+        int endIdx = 1;
 
-        while (end_index != N) {
+        while (endIdx != N) {
             if (sum == N) {
                 count++;
-                end_index++;
-                sum = sum + end_index;
+                endIdx++;
+                sum += endIdx;
             }
-            else if (sum > N) {
-                sum = sum - start_index;
-                start_index++;
+            else if (sum < N) {
+                endIdx++;
+                sum += endIdx;
             }
             else {
-                end_index++;
-                sum = sum + end_index;
+                sum -= startIdx;
+                startIdx++;
             }
         }
 
--- src/tmp.java
+++ src/tmp.java
@@ -4,29 +4,28 @@ public class tmp {
     public static void main(String args[]) {
         Scanner sc = new Scanner(System.in);
         int N = sc.nextInt();
-        int M = sc.nextInt();
-        long S[] = new long[N];
-        long C[] = new long[M];
-        long answer = 0;
 
-        S[0] = sc.nextInt();
-        for (int i = 1; i < N; i++) {
-            S[i] = S[i - 1] + sc.nextInt();
-        }
+        int count = 1;
+        int start_index = 1;
+        int end_index = 1;
+        int sum = 1;
 
-        for (int i = 0; i < N; i++) {
-            int remainder = (int) (S[i] % M);
-            if (remainder == 0) {
-                answer++;
+        while (end_index != N) {
+            if (sum == N) {
+                count++;
+                end_index++;
+                sum = sum + end_index;
             }
-            C[remainder]++;
-        }
-
-        for (int i = 0; i < M; i++) {
-            if (C[i] > 1) {
-                answer = answer + (C[i] * (C[i] - 1) / 2);
+            else if (sum > N) {
+                sum = sum - start_index;
+                start_index++;
+            }
+            else {
+                end_index++;
+                sum = sum + end_index;
             }
         }
-        System.out.println(answer);
+
+        System.out.println(count);
     }
 }
