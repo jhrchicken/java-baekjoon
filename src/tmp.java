6a11954a29c2b7928602f729fa980cf770711b54
harim <jhr.chicken@gmail.com>
2026-07-28 23:38:44 +0900

    12891번 DNA 비밀번호

--- src/tmp.java
+++ src/tmp.java
@@ -1,47 +1,105 @@
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
-import java.util.Arrays;
 import java.util.StringTokenizer;
 
 public class tmp {
-    public static void main(String args[]) throws IOException {
-        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        int N = Integer.parseInt(br.readLine());
+    static int checkArr[];
+    static int myArr[];
+    static int checkSecret;
 
-        long A[] = new long[N];
-        StringTokenizer st = new StringTokenizer(br.readLine());
-        for (int i = 0; i < N; i++) {
-            A[i] = Long.parseLong(st.nextToken());
-        }
-        Arrays.sort(A);
+    public static void main(String args[]) throws NumberFormatException, IOException {
+        BufferedReader bf = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(bf.readLine());
 
-        int count = 0;
-        for (int k = 0; k < N; k++) {
-            int start_index = 0;
-            int end_index = N - 1;
-            while (start_index < end_index) {
-                if (A[start_index] + A[end_index] == A[k]) {
-                    if (start_index != k && end_index != k) {
-                        count++;
-                        break;
-                    }
-                    else if (start_index == k) {
-                        start_index++;
-                    }
-                    else if (end_index == k) {
-                        end_index--;
-                    }
-                }
-                else if (A[start_index] + A[end_index] < A[k]) {
-                    start_index++;
-                }
-                else if (A[start_index] + A[end_index] > A[k]) {
-                    end_index--;
-                }
+        int S = Integer.parseInt(st.nextToken());
+        int P = Integer.parseInt(st.nextToken());
+        int result = 0;
+        char A[] = new char[S];
+        checkArr = new int[4];
+        myArr = new int[4];
+        checkSecret = 0;
+        A = bf.readLine().toCharArray();
+        st = new StringTokenizer(bf.readLine());
+        for (int i = 0; i < 4; i++) {
+            checkArr[i] = Integer.parseInt(st.nextToken());
+            if (checkArr[i] == 0) {
+                checkSecret++;
             }
         }
+        for (int i = 0;  i < P; i++) {
+            Add(A[i]);
+        }
+        if (checkSecret == 4) {
+            result++;
+        }
 
-        System.out.println(count);
+        for (int i = 0; i < S - P; i++) {
+            Add(A[i + P]);
+            Remove(A[i]);
+            if (checkSecret == 4) {
+                result++;
+            }
+        }
+        System.out.println(result);
+        bf.close();
+    }
+
+    private static void Add(char c) {
+        switch (c) {
+            case 'A' :
+                myArr[0]++;
+                if (myArr[0] == checkArr[0]) {
+                    checkSecret++;
+                }
+                break;
+            case 'C' :
+                myArr[1]++;
+                if (myArr[1] == checkArr[1]) {
+                    checkSecret++;
+                }
+                break;
+            case 'G' :
+                myArr[2]++;
+                if (myArr[2] == checkArr[2]) {
+                    checkSecret++;
+                }
+                break;
+            case 'T' :
+                myArr[3]++;
+                if (myArr[3] == checkArr[3]) {
+                    checkSecret++;
+                }
+                break;
+        }
+    }
+
+    private static void Remove(char c) {
+        switch (c) {
+            case 'A' :
+                if (myArr[0] == checkArr[0]) {
+                    checkSecret--;
+                }
+                myArr[0]--;
+                break;
+            case 'C' :
+                if (myArr[1] == checkArr[1]) {
+                    checkSecret--;
+                }
+                myArr[1]--;
+                break;
+            case 'G' :
+                if (myArr[2] == checkArr[2]) {
+                    checkSecret--;
+                }
+                myArr[2]--;
+                break;
+            case 'T' :
+                if (myArr[3] == checkArr[3]) {
+                    checkSecret--;
+                }
+                myArr[3]--;
+                break;
+        }
     }
 }
